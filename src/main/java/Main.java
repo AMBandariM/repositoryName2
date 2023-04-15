a5c77af30e32e57305ccc423f421569df39786a6
AMBandariM <ambandarim@gmail.com>
2023-04-15 13:50:19 -0700

    fibonacci added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(fibonacci(10));
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
